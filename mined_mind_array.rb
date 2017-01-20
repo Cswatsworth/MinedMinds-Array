4562f30c064893fdfe7000aaed84b47fadee8557
Chad Swatsworth <Chad Swatsworth>
2017-01-20 01:35:25 -0500

    3,5,15. TWO ERRORS

--- mined_mind_array.rb
+++ mined_mind_array.rb
@@ -1,9 +1,25 @@
 def create_mined_minds_array()
 	#create an empty array
 	mined_minds_array = []
+	
+	num = 1 
 	100.times do
-		#push an item into the array
-		mined_minds_array << 'array item'
+		
+		if num % 3 == 0
+			mined_minds_array << 'mined'
+		
+		elsif num % 5 == 0
+			mined_minds_array << 'minds'
+
+		elsif num % 15 == 0
+			mined_minds_array << 'minedminds'
+
+		else
+
+			#push an item into the array
+			mined_minds_array << num
+		num += 1
+		end
 	end
 	#return the array
 	mined_minds_array
--- test_mined_minds_array.rb
+++ test_mined_minds_array.rb
@@ -2,15 +2,32 @@ require 'minitest/autorun'
 require_relative 'mined_mind_array.rb'
 
 class TestMMFunctions < Minitest::Test
-	def test_one_equals_one
+	def test_one_equals_one()
 		assert_equal(1,1)
 	end
-	def test_function_returns_array()
+	
+	def test_function_returns_array
 		results = create_mined_minds_array()
 		assert_equal(Array,results.class)
 	end
-	def test_function_returns_array
-		results = create_mined_minds_array
+	
+	def test_function_array_length	
+		results = create_mined_minds_array()
 		assert_equal(100,results.length)
+	end
+	
+	def test_3_returns_mined
+		results = create_mined_minds_array()
+		assert_equal('mined',results[2])
+	end
+
+	def test_5_returns_minds
+		results = create_mined_minds_array
+		assert_equal('minds',results[4])
+	end
+
+	def test_15_returns_mined_minds
+		results = create_mined_minds_array
+		assert_equal('mined_minds',results[14])
 	end
 end
