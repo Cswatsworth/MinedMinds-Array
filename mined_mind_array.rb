43718d18cd994a45314d814f1fbec6e188b2abf7
Chad Swatsworth <Chad Swatsworth>
2017-01-20 13:59:03 -0500

    15-5-15 Array success

--- mined_mind_array.rb
+++ mined_mind_array.rb
@@ -5,21 +5,26 @@ def create_mined_minds_array()
 	num = 1 
 	100.times do
 		
-		if num % 3 == 0
-			mined_minds_array << 'mined'
+		if num % 15 == 0
+			mined_minds_array << 'mined_minds'
+			
 		
 		elsif num % 5 == 0
 			mined_minds_array << 'minds'
-
-		elsif num % 15 == 0
-			mined_minds_array << 'minedminds'
-
+			
+		
+		elsif num % 3 == 0
+			mined_minds_array << 'mined'
+			
+		
 		else
-
 			#push an item into the array
 			mined_minds_array << num
-		num += 1
+			
+		
 		end
+	
+		num += 1
 	end
 	#return the array
 	mined_minds_array
